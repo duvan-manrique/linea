a057c7c5186435ac2e3320a0833cdd176653d8e3
ASJKL26 <LOKUS.09@GMAIL.COM>
2021-11-24 00:44:41 -0500

    cambio de paquete a .war

--- src/main/java/com/duvan/linea/LineaApplication.java
+++ src/main/java/com/duvan/linea/LineaApplication.java
@@ -2,12 +2,20 @@ package com.duvan.linea;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LineaApplication {
+public class LineaApplication extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(LineaApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LineaApplication.class, args);
 	}
+	
 
 }
